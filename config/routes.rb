54446f2885e03e278dd571afbd304d91ce167611
Lucas Perin <lucasdafonsecaperin@gmail.com>
2025-01-21 19:33:09 -0300

    Adiciona autenticador

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  root "chatroom#index"
+  get "login", to: "sessions#new"
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
