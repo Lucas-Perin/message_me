e75bf2e82aebd8542c2754ecd4c19b4584893578
Lucas Perin <lucasdafonsecaperin@gmail.com>
2025-01-22 17:03:16 -0300

    Adiciona as mensagens do chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "messages/create"
   root "chatroom#index"
   get "login", to: "sessions#new"
   get "up" => "rails/health#show", as: :rails_health_check
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  post "message", to: "messages#create"
 end
