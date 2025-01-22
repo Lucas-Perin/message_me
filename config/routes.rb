96a91d3bcf676c2803907e65069d18b76db2a98f
Lucas Perin <lucasdafonsecaperin@gmail.com>
2025-01-22 17:18:38 -0300

    Criação de um canal de chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get "messages/create"
   root "chatroom#index"
+  get "messages/create"
   get "login", to: "sessions#new"
   get "up" => "rails/health#show", as: :rails_health_check
+
   post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
   post "message", to: "messages#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  mount ActionCable.server, at: "/cable"
 end
